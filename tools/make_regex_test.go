7d3c348bbc3d53ece3f0dee39609975cf24ce203
Kirill Grachev <kirill.grachoff@gmail.com>
2021-10-04 20:55:38 +0300

    error in tests: incorrect answer

--- tools/make_regex_test.go
+++ tools/make_regex_test.go
@@ -133,7 +133,7 @@ func BenchmarkMakeRegex(b *testing.B) {
 		edge.Edge{1, 2, "b"},
 	)
 
-	out := "()+(a)+(b)"
+	out := "()+(a)+(ab)+(b)"
 	for testNo := 0; testNo < b.N; testNo++ {
 		ans, err := MakeRegex(m)
 		if err != nil {
